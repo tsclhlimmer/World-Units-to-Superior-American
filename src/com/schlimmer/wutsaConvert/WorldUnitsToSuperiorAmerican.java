ca8e940727e4172f094d4ac136d87caea1c6c688
Wartz <wartzay@gmail.com>
2014-10-31 17:30:07 -0400

    Resturctured the conversion method in the WorldUnitsToSuperiorAmerican class to allow for adding more types of conversion methods in the future.

--- src/com/schlimmer/wutsaConvert/WorldUnitsToSuperiorAmerican.java
+++ src/com/schlimmer/wutsaConvert/WorldUnitsToSuperiorAmerican.java
@@ -3,9 +3,19 @@ package com.schlimmer.wutsaConvert;
 public class WorldUnitsToSuperiorAmerican {
 	
 	//declare variables
-	double inputMeters;
-	double inchesToCm = 2.54;
-	double cmToInches = 0.3937;
+	double unitValue;
+	String unitMeasurementType;
+	String unitConvertToType;
+	private double inchesToCm = 2.54;
+	private double cmToInches = 0.3937;
+	private String mile = "mi";
+	private String kilometer = "km";
+	private int miValue = 5280;
+	private int ftValue = 12;
+	private int inValue = 1;
+	private int kmValue = 1000;
+	private int mValue = 100;
+	private int cmValue = 1;
 	
 	//2 constructors, one to initialize variables to 0 and 1 of the same name to accept parameters
 	//Constructor
@@ -13,51 +23,42 @@ public class WorldUnitsToSuperiorAmerican {
 	//Initialize variables
 	public WorldUnitsToSuperiorAmerican()
 	{
-		inputMeters = 0;
+		unitValue = 0;
+		unitMeasurementType = "";
+		unitConvertToType = "";
 	}
-	public WorldUnitsToSuperiorAmerican(double input)
+	public WorldUnitsToSuperiorAmerican(double inputUnitValue, String inputUnitMeasurementType, String inputUnitConvertToType)
 	{
 		// TODO Auto-generated constructor stub
 		
-		inputMeters = input;
-		this.conversion(input);
-	}
-	
-	private double conversion(double inputMeters)
-	{
-		//1 meter = 3.28084 feet
-		double meterToFeet = 3.28084;
-		double conversion = (inputMeters * meterToFeet);
-		
-		return conversion;
-		
+		unitValue = inputUnitValue;
+		unitMeasurementType = inputUnitMeasurementType;
+		unitConvertToType = inputUnitConvertToType;
+		this.conversion(unitValue, unitMeasurementType, unitConvertToType);
 	}
 	
 	
-	//TODO implement this method in toString()
-	private double milesToKilometers(double inputMiles)
+	private double conversion(double unitValue, String unitMeasurementType, String unitConvertToType)
 	{
-		double milesToKilometers = 0.62137119;
-		double conversion = (inputMiles * milesToKilometers);
-		
+		//TODO: Create conversion methods and just call them here rather than hard coding each conversion
+		double conversion = 0;
+		if (unitMeasurementType == mile && unitConvertToType == kilometer)
+		{
+			conversion = (unitValue * miValue * ftValue * inchesToCm / mValue / kmValue);
+		}
 		return conversion;
 	}
+	
 	
 	public String toString()
 	{
-		return inputMeters + " meter(s) is equal to "  + this.conversion(inputMeters) + " feet"; //for now
-		
+		return unitValue + " <unitType> is equal to "  + this.conversion(unitValue, mile, kilometer) + " <convertedUnitType>"; //for now
 	}
 	
-	//public String display()
-	//{
-		//return System.out.println(this.toString());
-	//error: cannot return void result^
-	//}*//
 
 	public static void main(String[] args)
 	{
-		WorldUnitsToSuperiorAmerican convert = new WorldUnitsToSuperiorAmerican(1);
+		WorldUnitsToSuperiorAmerican convert = new WorldUnitsToSuperiorAmerican(1, "mi", "km");
 		
 		System.out.println(convert.toString());
 		
