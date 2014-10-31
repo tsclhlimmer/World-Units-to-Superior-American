b23bb681bc24c79d2cd90521233906db37db2469
Theresa Schlimmer <tas.flower@gmail.com>
2014-10-31 02:14:40 -0400

    latest

--- src/WorldUnitsToSuperiorAmerican.java
+++ src/WorldUnitsToSuperiorAmerican.java
@@ -4,23 +4,33 @@ public class WorldUnitsToSuperiorAmerican {
 	//declare variables
 	double inputMeters;
 	
+	
+	//Constructor
+	//Initialize variables
+	public WorldUnitsToSuperiorAmerican()
+	{
+		// TODO Auto-generated constructor stub
+		
+		inputMeters = 0;
+	}
+	
 	private double conversion(double inputMeters)
 	{
+		double conversionRate;
+		//1 meter = 3.28084 feet
+		double meterToFeet = 3.28084;
+		double conversion = (inputMeters * meterToFeet);
 		
-		double conversion = 
 		return conversion;
 		
 	}
 	
-	private String display(double display)
+	public String toString()
 	{
-		return null; //for now
+		return "conversion"; //for now
 		
 	}
 
-	public WorldUnitsToSuperiorAmerican()
-	{
-		// TODO Auto-generated constructor stub
-	}
+	
 
 }
