a74fd4bf9780b9a4222415ba30e7916505fa2270
Wartz <wartzay@gmail.com>
2014-10-31 02:54:36 -0400

    removed main method, added gui class

---
+++ src/WorldUnitsToSuperiorAmericanGUI.java
@@ -0,0 +1,46 @@
+
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class WorldUnitsToSuperiorAmericanGUI extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 823507040481624166L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WorldUnitsToSuperiorAmericanGUI frame = new WorldUnitsToSuperiorAmericanGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public WorldUnitsToSuperiorAmericanGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
